577460502dfac66b4311a3e9d650b48d99ebcc2f
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-04-08 10:45:30 +0300

    add get_all_tutorial_badges()
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Beautiful Soup Tutorial - Web Scraping in Python](https://www.youtube.com/watch?v=87Gx3U0BDlo)
+
+```bash
+$ pip install --upgrade pip setuptools requests bs4
+```
--- web_scraping.py
+++ web_scraping.py
@@ -1,12 +1,15 @@
+#!./.venv/bin/python
 """
 scraping online articles from realpython.com
 
-1. get all tags (badges) from main page of the site
-2. Open main page of each tag and get count of pages
-3. get articles from all pages of tag
-4. save articles in  text file (markdown)
-5. create one list with titles of all articles (+tags, + public date)
+- [x] get all tags (badges) from main page of the site
+- [ ] Open main page of each tag and get count of pages
+- [ ] get articles from all pages of tag
+- [ ] save articles in  text file (markdown)
+- [ ] create one list with titles of all articles (+tags, + public date)
 """
+
+import urllib
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,25 +18,12 @@ URL = 'https://realpython.com/'
 _CACHED_CONTENT = dict()
 
 
-def get_page(url: str) -> str:
+def get_page(url: str) -> bytes:
     if url not in _CACHED_CONTENT:
         result = requests.get(url)
         if result.status_code == 200:
             _CACHED_CONTENT[url] = result.content
     return _CACHED_CONTENT[url]
-
-
-def _main(url=URL) -> None:
-    src = get_page(url)
-    # print(src)
-    soup = BeautifulSoup(src, features='html.parser')
-    tutorial_topics_sidebars = soup.find_all("div", {"class": "sidebar-module"})
-    for sidebar in tutorial_topics_sidebars:
-        badge_tags = sidebar.find_all("a", {"class": "badge"}, href=True)
-        badges = dict()
-        for badge in badge_tags:
-            badges[badge.text] = badge.attrs['href']
-    print(badges)
 
 
 class Topic:
@@ -47,26 +37,43 @@ class Topic:
             self.tags[tag.text] = tag.attrs['href']
 
     def __str__(self):
-        result = (f"# {self.heading}\n"
+        result = (f"\n# {self.heading}\n"
                   f"{self.date} || {self.url}\n")
         for tag in self.tags.keys():
-            result = ' '.join((result, f"@{tag}"))
+            result = f"{result} @{tag}"
         return result
 
 
-def main(url=URL) -> None:
-    src = get_page(url)
-    soup = BeautifulSoup(src, features='html.parser')
+def get_main_page_articles(url):
+    src: bytes = get_page(url)
+    soup: BeautifulSoup = BeautifulSoup(src, features='html.parser')
     topics = soup.find_all(name='div', attrs={'class': 'card border-0'})
     topics_list = list()
     for topic in topics:
         heading = topic.find(name='h2', attrs={'class': 'card-title'}).text
-        topic_url = '/'.join((url.rstrip('/'), topic.a['href'].lstrip('/')))
+        topic_url = urllib.parse.urljoin(url, topic.a['href'])
         date = topic.find(name='span', attrs={'class': ['mr-2']}).text
         tags = topic.find_all(name='a', attrs={'class': 'badge'})
         topics_list.append(Topic(heading, topic_url, date, tags))
     for topic in topics_list:
         print(topic)
+
+
+def get_all_tutorial_badges(url: str) -> dict:
+    src: bytes = get_page(url)
+    soup: BeautifulSoup = BeautifulSoup(src, features='html.parser')
+    tutorial_topics_sidebars = soup.find_all("div", {"class": "sidebar-module"})
+    for sidebar in tutorial_topics_sidebars:
+        badge_tags = sidebar.find_all("a", {"class": "badge"}, href=True)
+        badges = dict()
+        for badge in badge_tags:
+            badges[badge.text] = urllib.parse.urljoin(url, badge.attrs['href'])
+    return badges
+
+
+def main(url=URL) -> None:
+    tags = get_all_tutorial_badges(url)
+    print(tags)
 
 
 if __name__ == '__main__':
